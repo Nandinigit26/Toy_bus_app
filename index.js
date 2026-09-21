32d69c376e9b6990ee96774cbd2c62e7c742ecca
Nandinigit26 <nandiniy2026@gmail.com>
2026-09-21 17:06:14 +0530

    Two new files modified and one new file added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 a=10
 b=20
 console.log(a+b)
+nandini="Hello, I am Nandini and I am learning GitHub tools and functionalities."
+baksha="Hello, I am Baksha and I am learning GitHub tools and functionalities."
+console.log(nandini)
+console.log(baksha)
